d969a7b34b33a84b0e29f385087cbaa23d93f4c8
NAVIN CHAUDHARY <154901815+Navin-Chaudhary@users.noreply.github.com>
2024-08-02 13:45:52 +0530

    git code push

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-
+import './Spiner'
 const Profile = ({ profile }) => {
   const [repos, setRepos] = useState([]);
 
