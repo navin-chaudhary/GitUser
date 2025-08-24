66714925c6cb9add4b5c6c50baf9c3354881bddc
Navin Chaudhary <hello@navin.bio>
2025-08-24 13:14:54 +0530

    resolve vercel deployment error

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
   images: {
-    unoptimized: true,
     domains: ['avatars.githubusercontent.com'],
   },
 }
